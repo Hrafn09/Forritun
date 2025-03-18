bc6a3524bd49fce31321f3f7c5e8cc47ce629198
Krummi <steinssonhrafn@gmail.com>
2025-03-18 15:35:30 +0000

    Added a button that reduces counter, added a button that increases counter by two, added a reset button and changed background color of page to white

--- Verkefni 4/script.js
+++ Verkefni 4/script.js
@@ -1,10 +1,31 @@
 // Finna takka og teljara
 let counter = 0;
 const counterDisplay = document.getElementById("counter");
-const button = document.getElementById("countButton");
+const countUpButton = document.getElementById("countUpButton");
+const countDownButton = document.getElementById("countDownButton");
+const countDoubleButton = document.getElementById("countDoubleButton")
+const countResetButton = document.getElementById("countResetButton")
 
-// Bæta við smellu-atburði
-button.addEventListener("click", function()  {
+// Bæta við smellu-atburði fyrir hækkun
+countUpButton.addEventListener("click", function()  {
     counter++;
     counterDisplay.textContent = counter;
+});
+
+// Bæta við smellu-atburði fyrir lækkun
+countDownButton.addEventListener("click", function()  {
+    counter--;
+    counterDisplay.textContent = counter;
 })
+
+// Bæta við smellu-atburði fyrir hækkun +2
+countDoubleButton.addEventListener("click", function()  {
+    counter += 2;
+    counterDisplay.textContent = counter;
+});
+
+// Bæta við smellu-atburði fyrir endurstillingu
+countResetButton.addEventListener("click", function()  {
+    counter = 0;
+    counterDisplay.textContent = counter;
+});
