8d5554402975cd6eae8767b33ee6d3a70918e52a
Krummi <steinssonhrafn@gmail.com>
2025-03-28 14:59:38 +0000

    python

---
+++ Python verkefni/main.py
@@ -0,0 +1,14 @@
+
+nafn = input("Hvað heitir þú? ")
+aldur = input("Hvað ertu gamall? ")
+
+if aldur > ("20"):
+    print("Þetta nálgast lokin hjá þér")
+else:
+    print("Flott")
+
+
+
+
+print("Velkominn,", nafn)
+print("Þú ert", aldur, "ára")
